11e85fc3879670833a70575fe906aef18690c568
Martin Billinger <flkazemakase@gmail.com>
2022-05-05 20:54:21 +0200

    generalize gates over Logic trait

--- src/chapter01_boolean_logic.rs
+++ src/chapter01_boolean_logic.rs
@@ -6,35 +6,45 @@ pub enum Bool {
     I,
 }
 
-pub fn nand(a: Bool, b: Bool) -> Bool {
-    match (a, b) {
-        (I, I) => O,
-        _ => I,
+pub trait Logic {
+    fn nand(a: &Self, b: &Self) -> Self;
+}
+
+impl Logic for Bool {
+    fn nand(a: &Self, b: &Self) -> Self {
+        match (a, b) {
+            (I, I) => O,
+            _ => I,
+        }
     }
 }
 
-pub fn not(a: Bool) -> Bool {
+pub fn nand<T: Logic>(a: &T, b: &T) -> T {
+    T::nand(a, b)
+}
+
+pub fn not<T: Logic>(a: &T) -> T {
     nand(a, a)
 }
 
-pub fn and(a: Bool, b: Bool) -> Bool {
-    not(nand(a, b))
+pub fn and<T: Logic>(a: &T, b: &T) -> T {
+    not(&nand(a, b))
 }
 
-pub fn or(a: Bool, b: Bool) -> Bool {
-    nand(not(a), not(b))
+pub fn or<T: Logic>(a: &T, b: &T) -> T {
+    nand(&not(a), &not(b))
 }
 
-pub fn xor(a: Bool, b: Bool) -> Bool {
-    nand(nand(a, not(b)), nand(not(a), b))
+pub fn xor<T: Logic>(a: &T, b: &T) -> T {
+    nand(&nand(a, &not(b)), &nand(&not(a), b))
 }
 
-pub fn mux(a: Bool, b: Bool, sel: Bool) -> Bool {
-    nand(nand(sel, b), nand(not(sel), a))
+pub fn mux<T: Logic>(a: &T, b: &T, sel: &T) -> T {
+    nand(&nand(sel, b), &nand(&not(sel), a))
 }
 
-pub fn demux(i: Bool, sel: Bool) -> (Bool, Bool) {
-    let a = and(i, not(sel));
+pub fn demux<T: Logic>(i: &T, sel: &T) -> (T, T) {
+    let a = and(i, &not(sel));
     let b = and(i, sel);
     (a, b)
 }
@@ -45,59 +55,64 @@ mod tests {
 
     #[test]
     fn nand_gate() {
-        assert_eq!(nand(O, O), I);
-        assert_eq!(nand(O, I), I);
-        assert_eq!(nand(I, O), I);
-        assert_eq!(nand(I, I), O);
+        assert_eq!(nand(&O, &O), I);
+        assert_eq!(nand(&O, &I), I);
+        assert_eq!(nand(&I, &O), I);
+        assert_eq!(nand(&I, &I), O);
     }
 
     #[test]
     fn not_gate() {
-        assert_eq!(not(O), I);
-        assert_eq!(not(I), O);
+        assert_eq!(not(&O), I);
+        assert_eq!(not(&I), O);
     }
 
     #[test]
     fn and_gate() {
-        assert_eq!(and(O, O), O);
-        assert_eq!(and(O, I), O);
-        assert_eq!(and(I, O), O);
-        assert_eq!(and(I, I), I);
+        assert_eq!(and(&O, &O), O);
+        assert_eq!(and(&O, &I), O);
+        assert_eq!(and(&I, &O), O);
+        assert_eq!(and(&I, &I), I);
     }
 
     #[test]
     fn or_gate() {
-        assert_eq!(or(O, O), O);
-        assert_eq!(or(O, I), I);
-        assert_eq!(or(I, O), I);
-        assert_eq!(or(I, I), I);
+        assert_eq!(or(&O, &O), O);
+        assert_eq!(or(&O, &I), I);
+        assert_eq!(or(&I, &O), I);
+        assert_eq!(or(&I, &I), I);
     }
 
     #[test]
     fn xor_gate() {
-        assert_eq!(xor(O, O), O);
-        assert_eq!(xor(O, I), I);
-        assert_eq!(xor(I, O), I);
-        assert_eq!(xor(I, I), O);
+        assert_eq!(xor(&O, &O), O);
+        assert_eq!(xor(&O, &I), I);
+        assert_eq!(xor(&I, &O), I);
+        assert_eq!(xor(&I, &I), O);
     }
 
     #[test]
     fn mux_gate() {
-        assert_eq!(mux(O, O, O), O);
-        assert_eq!(mux(O, I, O), O);
-        assert_eq!(mux(I, O, O), I);
-        assert_eq!(mux(I, I, O), I);
-        assert_eq!(mux(O, O, I), O);
-        assert_eq!(mux(O, I, I), I);
-        assert_eq!(mux(I, O, I), O);
-        assert_eq!(mux(I, I, I), I);
+        assert_eq!(mux(&O, &O, &O), O);
+        assert_eq!(mux(&O, &I, &O), O);
+        assert_eq!(mux(&I, &O, &O), I);
+        assert_eq!(mux(&I, &I, &O), I);
+        assert_eq!(mux(&O, &O, &I), O);
+        assert_eq!(mux(&O, &I, &I), I);
+        assert_eq!(mux(&I, &O, &I), O);
+        assert_eq!(mux(&I, &I, &I), I);
     }
 
     #[test]
     fn demux_gate() {
-        assert_eq!(demux(O, O), (O, O));
-        assert_eq!(demux(O, I), (O, O));
-        assert_eq!(demux(I, O), (I, O));
-        assert_eq!(demux(I, I), (O, I));
+        assert_eq!(demux(&O, &O), (O, O));
+        assert_eq!(demux(&O, &I), (O, O));
+        assert_eq!(demux(&I, &O), (I, O));
+        assert_eq!(demux(&I, &I), (O, I));
     }
+
+    /*#[test]
+    fn multi_bit_not() {
+        assert_eq!()
+    }*/
 }
