f5eaf6ed22876b85cc524db9d099941e934278e6
Martin Billinger <flkazemakase@gmail.com>
2022-05-31 22:59:17 +0200

    switch to binary instruction representation

--- src/chapter06_assembler/code.rs
+++ src/chapter06_assembler/code.rs
@@ -1,66 +1,66 @@
 use crate::chapter06_assembler::parser::{Comp, Dest, Jump};
 
 impl Dest {
-    pub fn code(&self) -> &'static str {
+    pub fn code(&self) -> u16 {
         match self {
-            Dest::None => "000",
-            Dest::M => "001",
-            Dest::D => "010",
-            Dest::DM => "011",
-            Dest::A => "100",
-            Dest::AM => "101",
-            Dest::AD => "110",
-            Dest::ADM => "111",
+            Dest::None => 0b000,
+            Dest::M => 0b001,
+            Dest::D => 0b010,
+            Dest::DM => 0b011,
+            Dest::A => 0b100,
+            Dest::AM => 0b101,
+            Dest::AD => 0b110,
+            Dest::ADM => 0b111,
         }
     }
 }
 
 impl Jump {
-    pub fn code(&self) -> &'static str {
+    pub fn code(&self) -> u16 {
         match self {
-            Jump::None => "000",
-            Jump::GreaterZero => "001",
-            Jump::EqualZero => "010",
-            Jump::NotNegative => "011",
-            Jump::Negative => "100",
-            Jump::NotZero => "101",
-            Jump::NegativeOrZero => "110",
-            Jump::Unconditional => "111",
+            Jump::None => 0b000,
+            Jump::GreaterZero => 0b001,
+            Jump::EqualZero => 0b010,
+            Jump::NotNegative => 0b011,
+            Jump::Negative => 0b100,
+            Jump::NotZero => 0b101,
+            Jump::NegativeOrZero => 0b110,
+            Jump::Unconditional => 0b111,
         }
     }
 }
 
 impl Comp {
-    pub fn code(&self) -> &'static str {
+    pub fn code(&self) -> u16 {
         match self {
-            Comp::Zero => "0101010",
-            Comp::One => "0111111",
-            Comp::MinusOne => "0111010",
-            Comp::D => "0001100",
-            Comp::A => "0110000",
-            Comp::M => "1110000",
-            Comp::NotD => "0001101",
-            Comp::NotA => "0110001",
-            Comp::NotM => "1110001",
-            Comp::NegD => "0001111",
-            Comp::NegA => "0110011",
-            Comp::NegM => "1110011",
-            Comp::IncD => "0011111",
-            Comp::IncA => "0110111",
-            Comp::IncM => "1110111",
-            Comp::DecD => "0001110",
-            Comp::DecA => "0110010",
-            Comp::DecM => "1110010",
-            Comp::DplusA => "0000010",
-            Comp::DplusM => "1000010",
-            Comp::DminusA => "0010011",
-            Comp::DminusM => "1010011",
-            Comp::AminusD => "0000111",
-            Comp::MminusD => "1000111",
-            Comp::DandA => "0000000",
-            Comp::DandM => "1000000",
-            Comp::DorA => "0010101",
-            Comp::DorM => "0010101",
+            Comp::Zero => 0b0101010,
+            Comp::One => 0b0111111,
+            Comp::MinusOne => 0b0111010,
+            Comp::D => 0b0001100,
+            Comp::A => 0b0110000,
+            Comp::M => 0b1110000,
+            Comp::NotD => 0b0001101,
+            Comp::NotA => 0b0110001,
+            Comp::NotM => 0b1110001,
+            Comp::NegD => 0b0001111,
+            Comp::NegA => 0b0110011,
+            Comp::NegM => 0b1110011,
+            Comp::IncD => 0b0011111,
+            Comp::IncA => 0b0110111,
+            Comp::IncM => 0b1110111,
+            Comp::DecD => 0b0001110,
+            Comp::DecA => 0b0110010,
+            Comp::DecM => 0b1110010,
+            Comp::DplusA => 0b0000010,
+            Comp::DplusM => 0b1000010,
+            Comp::DminusA => 0b0010011,
+            Comp::DminusM => 0b1010011,
+            Comp::AminusD => 0b0000111,
+            Comp::MminusD => 0b1000111,
+            Comp::DandA => 0b0000000,
+            Comp::DandM => 0b1000000,
+            Comp::DorA => 0b0010101,
+            Comp::DorM => 0b0010101,
         }
     }
 }
@@ -71,57 +71,57 @@ mod tests {
 
     #[test]
     fn dest_mnemonics() {
-        assert_eq!(Dest::None.code(), "000");
-        assert_eq!(Dest::M.code(), "001");
-        assert_eq!(Dest::D.code(), "010");
-        assert_eq!(Dest::DM.code(), "011");
-        assert_eq!(Dest::A.code(), "100");
-        assert_eq!(Dest::AM.code(), "101");
-        assert_eq!(Dest::AD.code(), "110");
-        assert_eq!(Dest::ADM.code(), "111");
+        assert_eq!(Dest::None.code(), 0b000);
+        assert_eq!(Dest::M.code(), 0b001);
+        assert_eq!(Dest::D.code(), 0b010);
+        assert_eq!(Dest::DM.code(), 0b011);
+        assert_eq!(Dest::A.code(), 0b100);
+        assert_eq!(Dest::AM.code(), 0b101);
+        assert_eq!(Dest::AD.code(), 0b110);
+        assert_eq!(Dest::ADM.code(), 0b111);
     }
 
     #[test]
     fn jump_mnemonics() {
-        assert_eq!(Jump::None.code(), "000");
-        assert_eq!(Jump::GreaterZero.code(), "001");
-        assert_eq!(Jump::EqualZero.code(), "010");
-        assert_eq!(Jump::NotNegative.code(), "011");
-        assert_eq!(Jump::Negative.code(), "100");
-        assert_eq!(Jump::NotZero.code(), "101");
-        assert_eq!(Jump::NegativeOrZero.code(), "110");
-        assert_eq!(Jump::Unconditional.code(), "111");
+        assert_eq!(Jump::None.code(), 0b000);
+        assert_eq!(Jump::GreaterZero.code(), 0b001);
+        assert_eq!(Jump::EqualZero.code(), 0b010);
+        assert_eq!(Jump::NotNegative.code(), 0b011);
+        assert_eq!(Jump::Negative.code(), 0b100);
+        assert_eq!(Jump::NotZero.code(), 0b101);
+        assert_eq!(Jump::NegativeOrZero.code(), 0b110);
+        assert_eq!(Jump::Unconditional.code(), 0b111);
     }
 
     #[test]
     fn comp_mnemonics() {
-        assert_eq!(Comp::Zero.code(), "0101010");
-        assert_eq!(Comp::One.code(), "0111111");
-        assert_eq!(Comp::MinusOne.code(), "0111010");
-        assert_eq!(Comp::D.code(), "0001100");
-        assert_eq!(Comp::A.code(), "0110000");
-        assert_eq!(Comp::M.code(), "1110000");
-        assert_eq!(Comp::NotD.code(), "0001101");
-        assert_eq!(Comp::NotA.code(), "0110001");
-        assert_eq!(Comp::NotM.code(), "1110001");
-        assert_eq!(Comp::NegD.code(), "0001111");
-        assert_eq!(Comp::NegA.code(), "0110011");
-        assert_eq!(Comp::NegM.code(), "1110011");
-        assert_eq!(Comp::IncD.code(), "0011111");
-        assert_eq!(Comp::IncA.code(), "0110111");
-        assert_eq!(Comp::IncM.code(), "1110111");
-        assert_eq!(Comp::DecD.code(), "0001110");
-        assert_eq!(Comp::DecA.code(), "0110010");
-        assert_eq!(Comp::DecM.code(), "1110010");
-        assert_eq!(Comp::DplusA.code(), "0000010");
-        assert_eq!(Comp::DplusM.code(), "1000010");
-        assert_eq!(Comp::DminusA.code(), "0010011");
-        assert_eq!(Comp::DminusM.code(), "1010011");
-        assert_eq!(Comp::AminusD.code(), "0000111");
-        assert_eq!(Comp::MminusD.code(), "1000111");
-        assert_eq!(Comp::DandA.code(), "0000000");
-        assert_eq!(Comp::DandM.code(), "1000000");
-        assert_eq!(Comp::DorA.code(), "0010101");
-        assert_eq!(Comp::DorM.code(), "0010101");
+        assert_eq!(Comp::Zero.code(), 0b0101010);
+        assert_eq!(Comp::One.code(), 0b0111111);
+        assert_eq!(Comp::MinusOne.code(), 0b0111010);
+        assert_eq!(Comp::D.code(), 0b0001100);
+        assert_eq!(Comp::A.code(), 0b0110000);
+        assert_eq!(Comp::M.code(), 0b1110000);
+        assert_eq!(Comp::NotD.code(), 0b0001101);
+        assert_eq!(Comp::NotA.code(), 0b0110001);
+        assert_eq!(Comp::NotM.code(), 0b1110001);
+        assert_eq!(Comp::NegD.code(), 0b0001111);
+        assert_eq!(Comp::NegA.code(), 0b0110011);
+        assert_eq!(Comp::NegM.code(), 0b1110011);
+        assert_eq!(Comp::IncD.code(), 0b0011111);
+        assert_eq!(Comp::IncA.code(), 0b0110111);
+        assert_eq!(Comp::IncM.code(), 0b1110111);
+        assert_eq!(Comp::DecD.code(), 0b0001110);
+        assert_eq!(Comp::DecA.code(), 0b0110010);
+        assert_eq!(Comp::DecM.code(), 0b1110010);
+        assert_eq!(Comp::DplusA.code(), 0b0000010);
+        assert_eq!(Comp::DplusM.code(), 0b1000010);
+        assert_eq!(Comp::DminusA.code(), 0b0010011);
+        assert_eq!(Comp::DminusM.code(), 0b1010011);
+        assert_eq!(Comp::AminusD.code(), 0b0000111);
+        assert_eq!(Comp::MminusD.code(), 0b1000111);
+        assert_eq!(Comp::DandA.code(), 0b0000000);
+        assert_eq!(Comp::DandM.code(), 0b1000000);
+        assert_eq!(Comp::DorA.code(), 0b0010101);
+        assert_eq!(Comp::DorM.code(), 0b0010101);
     }
 }
