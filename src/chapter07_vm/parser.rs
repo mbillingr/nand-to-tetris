649ef693acc219c5bd363146c96d55dbe5f672b3
Martin Billinger <flkazemakase@gmail.com>
2022-06-03 12:49:13 +0200

    (wip) vm parser

---
+++ src/chapter07_vm/parser.rs
@@ -0,0 +1,120 @@
+use crate::chapter06_assembler::parser::FromStrNocopy;
+use std::str::FromStr;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Command {
+    Arithmetic(ArithmeticCmd),
+    Push(Segment, usize),
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ArithmeticCmd {
+    Add,
+    Sub,
+    Neg,
+    Eq,
+    Gt,
+    Lt,
+    And,
+    Or,
+    Not,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Segment {
+    Argument,
+    Local,
+    Static,
+    Constant,
+    This,
+    That,
+    Pointer,
+    Temp,
+}
+
+impl<'s> FromStrNocopy<'s> for Command {
+    fn from_str(s: &'s str) -> Result<Self, String> {
+        if let Ok(ac) = ArithmeticCmd::from_str(s) {
+            return Ok(Command::Arithmetic(ac));
+        }
+
+        let tokens: Vec<_> = s.split_whitespace().collect();
+        match tokens.as_slice() {
+            ["push", segment, index] => Ok(Command::Push(
+                Segment::from_str(segment)?,
+                FromStrNocopy::from_str(index)?,
+            )),
+            _ => todo!(),
+        }
+    }
+}
+
+impl FromStr for ArithmeticCmd {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, ()> {
+        match s {
+            "add" => Ok(ArithmeticCmd::Add),
+            "sub" => Ok(ArithmeticCmd::Sub),
+            "neg" => Ok(ArithmeticCmd::Neg),
+            "eq" => Ok(ArithmeticCmd::Eq),
+            "gt" => Ok(ArithmeticCmd::Gt),
+            "lt" => Ok(ArithmeticCmd::Lt),
+            "and" => Ok(ArithmeticCmd::And),
+            "or" => Ok(ArithmeticCmd::Or),
+            "not" => Ok(ArithmeticCmd::Not),
+            _ => Err(()),
+        }
+    }
+}
+
+impl<'s> FromStrNocopy<'s> for Segment {
+    fn from_str(s: &'s str) -> Result<Self, String> {
+        match s {
+            "this" => Ok(Segment::This),
+            _ => Err(format!("Invalid segment {}", s)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ArithmeticCmd::*;
+    use Command::*;
+    use Segment::*;
+
+    #[test]
+    fn parse_arithmetic() {
+        assert_eq!(ArithmeticCmd::from_str("add"), Ok(Add));
+        assert_eq!(ArithmeticCmd::from_str("sub"), Ok(Sub));
+        assert_eq!(ArithmeticCmd::from_str("neg"), Ok(Neg));
+        assert_eq!(ArithmeticCmd::from_str("eq"), Ok(Eq));
+        assert_eq!(ArithmeticCmd::from_str("gt"), Ok(Gt));
+        assert_eq!(ArithmeticCmd::from_str("lt"), Ok(Lt));
+        assert_eq!(ArithmeticCmd::from_str("and"), Ok(And));
+        assert_eq!(ArithmeticCmd::from_str("or"), Ok(Or));
+        assert_eq!(ArithmeticCmd::from_str("not"), Ok(Not));
+    }
+
+    #[test]
+    fn parse_segment() {
+        assert_eq!(Segment::from_str("argument"), Ok(Argument));
+        assert_eq!(Segment::from_str("local"), Ok(Local));
+        assert_eq!(Segment::from_str("static"), Ok(Static));
+        assert_eq!(Segment::from_str("constant"), Ok(Constant));
+        assert_eq!(Segment::from_str("this"), Ok(This));
+        assert_eq!(Segment::from_str("that"), Ok(That));
+        assert_eq!(Segment::from_str("pointer"), Ok(Pointer));
+        assert_eq!(Segment::from_str("temp"), Ok(Temp));
+    }
+
+    #[test]
+    fn parse_arithmetic_command() {
+        assert_eq!(Command::from_str("add"), Ok(Arithmetic(Add)));
+    }
+
+    #[test]
+    fn parse_push_command() {
+        assert_eq!(Command::from_str("push this 0"), Ok(Push(This, 0)));
+    }
+}
