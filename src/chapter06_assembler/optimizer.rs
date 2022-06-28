1398eb29e35659d1457b474272b3f3f23e98246f
Martin Billinger <mbillingr@users.noreply.github.com>
2022-06-28 21:01:13 +0200

    implement a few optimization rules

--- src/chapter06_assembler/optimizer.rs
+++ src/chapter06_assembler/optimizer.rs
@@ -14,7 +14,11 @@ pub struct PeepholeOptimizer<'s> {
 impl<'s> Default for PeepholeOptimizer<'s> {
     fn default() -> Self {
         PeepholeOptimizer {
-            rules: vec![Box::new(IndirectAssignment)],
+            rules: vec![
+                Box::new(IndirectAssignment),
+                Box::new(TrivialConstant4),
+                Box::new(TrivialConstant5),
+            ],
         }
     }
 }
@@ -77,6 +81,69 @@ impl<'s> PeepholeRule<'s> for IndirectAssignment {
     }
 }
 
+struct TrivialConstant4;
+
+impl<'s> PeepholeRule<'s> for TrivialConstant4 {
+    fn pattern_length(&self) -> usize {
+        4
+    }
+
+    fn apply(&self, ops: &[Instruction<'s>]) -> Option<Vec<Instruction<'s>>> {
+        match ops {
+            [A(c @ ("0" | "1" | "-1")), C(Dest::D, Comp::A, Jump::None), A(ptr), C(dest, Comp::D, jmp)] =>
+            {
+                let comp = match *c {
+                    "0" => Comp::Zero,
+                    "1" => Comp::One,
+                    "-1" => Comp::MinusOne,
+                    _ => unreachable!(),
+                };
+                Some(vec![A(ptr), C(*dest, comp, *jmp)])
+            }
+            _ => None,
+        }
+    }
+}
+
+struct TrivialConstant5;
+
+impl<'s> PeepholeRule<'s> for TrivialConstant5 {
+    fn pattern_length(&self) -> usize {
+        5
+    }
+
+    fn apply(&self, ops: &[Instruction<'s>]) -> Option<Vec<Instruction<'s>>> {
+        match ops {
+            [A(c @ ("0" | "1" | "-1")), C(Dest::D, Comp::A, Jump::None), A(ptr), middle @ C(
+                Dest::A | Dest::M | Dest::AM,
+                Comp::M
+                | Comp::A
+                | Comp::IncM
+                | Comp::IncA
+                | Comp::DecM
+                | Comp::DecA
+                | Comp::NegA
+                | Comp::NegM
+                | Comp::NotA
+                | Comp::NotM
+                | Comp::One
+                | Comp::Zero
+                | Comp::MinusOne,
+                Jump::None,
+            ), C(dest, Comp::D, jmp)] => {
+                let comp = match *c {
+                    "0" => Comp::Zero,
+                    "1" => Comp::One,
+                    "-1" => Comp::MinusOne,
+                    _ => unreachable!(),
+                };
+                Some(vec![A(ptr), *middle, C(*dest, comp, *jmp)])
+            }
+            _ => None,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -84,8 +151,8 @@ mod tests {
     #[test]
     fn indirect_assignment() {
         let code = vec![
-            C(Dest::A, Comp::M, Jump::None), // A3 = *A2
-            C(Dest::D, Comp::A, Jump::None), // D0 = A3
+            C(Dest::A, Comp::M, Jump::None),
+            C(Dest::D, Comp::A, Jump::None),
             A("ARG"),
         ];
 
@@ -93,5 +160,43 @@ mod tests {
         let result = opt.optimize(code);
 
         assert_eq!(result, [C(Dest::D, Comp::M, Jump::None), A("ARG"),]);
+    }
+
+    #[test]
+    fn trivial_constant4() {
+        let code = vec![
+            A("0"),
+            C(Dest::D, Comp::A, Jump::None),
+            A("ARG"),
+            C(Dest::M, Comp::D, Jump::None),
+        ];
+
+        let opt = PeepholeOptimizer::default();
+        let result = opt.optimize(code);
+
+        assert_eq!(result, [A("ARG"), C(Dest::M, Comp::Zero, Jump::None),]);
+    }
+
+    #[test]
+    fn trivial_constant5() {
+        let code = vec![
+            A("0"),
+            C(Dest::D, Comp::A, Jump::None),
+            A("ARG"),
+            C(Dest::A, Comp::IncM, Jump::None),
+            C(Dest::M, Comp::D, Jump::None),
+        ];
+
+        let opt = PeepholeOptimizer::default();
+        let result = opt.optimize(code);
+
+        assert_eq!(
+            result,
+            [
+                A("ARG"),
+                C(Dest::A, Comp::IncM, Jump::None),
+                C(Dest::M, Comp::Zero, Jump::None),
+            ]
+        );
     }
 }
